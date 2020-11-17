eed9bfe7c8fe78474c31efca3cee987bc06cd484
Andrey Stepanov <an.stepanov@skoltech.ru>
2020-11-17 12:45:56 +0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b1020ba9b937c62e3e87";
+/******/ 	var hotCurrentHash = "1f2219df5178208520fd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- _next/static/j5lZ8rWIg9gzRkQ0xGX6v/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F4e5edfe1e4c6ae8bad8f.css","static\u002Fchunks\u002Fpages\u002Findex-f58b6febeff13e6b367d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
