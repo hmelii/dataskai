b374994cc975f0eeae429eef331fc59760ac816c
Andrey Stepanov <an.stepanov@skoltech.ru>
2020-11-20 13:59:19 +0300

    Updates

--- _next/static/vb_aKM8Rp57Ps2ujgHC7l/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fbeefb948ee20e3c4c745.css","static\u002Fchunks\u002Fpages\u002Findex-d74ba7d55d3c44556c27.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
