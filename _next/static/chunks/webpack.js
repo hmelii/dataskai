9d507674101544d2bef819e930668da65f6a391f
Andrey Stepanov <an.stepanov@skoltech.ru>
2020-12-02 21:21:54 +0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0127a88bfcb52b7ba4ca";
+/******/ 	var hotCurrentHash = "e2cddd43211e626689d2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
