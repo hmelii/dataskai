52718b846a47e0fd182029315d185f368064f096
Andrey Stepanov <an.stepanov@skoltech.ru>
2020-12-17 15:12:25 +0300

    поправил цвет фавиконки в сафари

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3eadd7d995677e08ba17";
+/******/ 	var hotCurrentHash = "fc77781eb267aafe5db9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
