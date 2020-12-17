3e9baef51fb37cfca689c1ef109a3d5eef60d51e
Andrey Stepanov <an.stepanov@skoltech.ru>
2020-12-17 13:52:51 +0300

    Добавил фавиконки

--- layouts/Layout.jsx
+++ layouts/Layout.jsx
@@ -4,23 +4,30 @@ import Wrapper from "./wrapper/Wrapper";
 import Footer from "./footer/Footer";
 import Content from "./content/Content";
 
-export default function Layot({ children } ) {
+export default function Layot({ children }) {
 
   return (
     <>
       <Head>
         <title>DATASKAI</title>
         <link rel="icon" href="/favicon.ico"/>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta charSet="utf-8"/>
+        <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <script type="text/javascript" src="asciinema-player.js"></script>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
       <Wrapper>
 
         <Header/>
         <Content>
-          { children }
+          {children}
         </Content>
 
         <Footer/>
